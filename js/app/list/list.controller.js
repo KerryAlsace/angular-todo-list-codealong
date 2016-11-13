429e94c5c1e0e1cc5c37d77415e42777d37487b2
KerryAlsace <kerrydougherty@gmail.com>
2016-11-13 12:11:03 -0500

    add to index.html

---
+++ js/app/list/list.controller.js
@@ -0,0 +1,26 @@
+angular
+  .module('todoApp')
+  .controller('ListController', ListController);
+
+function ListController() {
+  var vm = this;
+
+  vm.list = {
+    [
+      name: "Todo List",
+      tasks: [
+        {
+          id: 1,
+          name: "Take out trash",
+          complete: false
+        },
+        {
+          id: 2,
+          name: "Watch tv",
+          complete: true
+        }
+      ]
+    ]
+  }
+
+}
